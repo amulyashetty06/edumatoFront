43bcebc51152f6bb98a47dcec028db4686beb755
Amulya <amulya.s.shetty@gmail.com>
2020-09-23 18:59:12 +0530

    a

---
+++ build/precache-manifest.5ba923f61026a14b22f0b72e53c0ee06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b95b286c468da35713bb25a154246f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f849dbad2611c6f7cdfb",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "1b4b8fab9f39b95dd433",
+    "url": "/static/css/main.439bf02c.chunk.css"
+  },
+  {
+    "revision": "f849dbad2611c6f7cdfb",
+    "url": "/static/js/2.a8415090.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8415090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4b8fab9f39b95dd433",
+    "url": "/static/js/main.4dd3436b.chunk.js"
+  },
+  {
+    "revision": "0529a98c11a206755e39",
+    "url": "/static/js/runtime-main.99490353.js"
+  }
+]);
